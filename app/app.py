26115af43f66d62fc7a3f2bbd54a90b317dfcf90
mandatory-username <106693160+hannaking@users.noreply.github.com>
2024-10-17 12:16:00 -0400

    set up fin facts to pull from a json

--- app/app.py
+++ app/app.py
@@ -5,27 +5,11 @@ import json
 app = Flask(__name__)
 cors = CORS(app)
 
-@app.route("/csc342groups")
-def csc_342_groups():
-    groups = {1: {'Content Specialist': ['Julia Hogg'],
-        'Designer': ['Sage DeVore'],
-        'Programmer(s)': ['Miles Dame', 'Hanna King']},
-        2: {'Content Specialist': ['Brady Fleuette'],
-        'Designer': ['David Lash'],
-        'Programmer(s)': ['Kyler Bailey', 'Brodee Clontz']},
-        3: {'Content Specialist': ['AJ Leary'],
-        'Designer': ['Khalid Ismail'],
-        'Programmer(s)': ['Matt Keller', 'Moultrie Dangerfield']},
-        4: {'Content Specialist': ['Collin Riddle'],
-        'Designer': ['Case Riddle'],
-        'Programmer(s)': ['Ellie Johnson', 'Jack Roberts']},
-        5: {'Content Specialist': ['Olivia Longsworth'],
-        'Designer': ['Flynn Nisbet'],
-        'Programmer(s)': ['Mengsrun Nit']},
-        6: {'Content Specialist': ['Jack Patterson'],
-        'Designer': ['Charlie Fink'],
-        'Programmer(s)': ['Emirhan Gencer']}}
-    return jsonify(groups)
+@app.route("/finFacts")
+def facts():
+    with open('facts.json') as f:
+        fin = json.load(f)
+    return jsonify(fin)
 
 @app.route("/anatomy")
 def anatomy():
