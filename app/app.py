f5bd66f2f10c497b7ceceb90554f90a5ada39d6c
mandatory-username <106693160+hannaking@users.noreply.github.com>
2024-10-30 13:00:57 -0400

    templates
    improvements: renamed home to index for convention, fixed a broken nav link, added dolphin types section with flexbox, and added voyager map

--- app/app.py
+++ app/app.py
@@ -1,20 +1,32 @@
-from flask import Flask, jsonify, 
+from flask import Flask, jsonify, json, render_template
 
 app = Flask(__name__)
-<<<<<<< Updated upstream
-cors = CORS(app)
 
 @app.route("/finFacts")
 def facts():
     with open('facts.json') as f:
         fin = json.load(f)
     return jsonify(fin)
-=======
->>>>>>> Stashed changes
 
 @app.route("/anatomy")
 def anatomy():
     with open('anatomy.json') as f:
         dolphin_data = json.load(f)
-    return jsonify
-# consulted AI for JS only
+    return jsonify(dolphin_data)
+
+@app.route("/types/<type>")
+def make_type_page(type):
+    with open('types.json') as f:
+        dolphin_type_data = json.load(f)
+
+    info = dolphin_type_data[type]
+    return render_template('types.html',
+                           title = info["common_name"],
+                           heading = info["common_name"],
+                           common_name = info["common_name"],
+                           sci_name = info["sci_name"],
+                           length = info["length"],
+                           weight = info["weight"],
+                           life_span = info["life_span"],
+                           desc = info["desc"]
+                           )
