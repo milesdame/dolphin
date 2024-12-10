a0b75db35e481843a9bcd4385ea51e4ced803b78
Miles Dame <milesedame@gmail.com>
2024-12-10 16:19:54 -0500

    Change directory

--- start_gunicorn.sh
+++ start_gunicorn.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cd /Users/rdrucker/Documents/dolphin/app
-source ../mgvenv/bin/activate
+cd /Users/milesdame/Documents/dolphin/app
+source ../.venv/bin/activate
 gunicorn --reload --workers 3 --capture-output --log-level debug --bind unix:dolphin.sock -m 007 wsgi:app
