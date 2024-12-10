45c72bf632eac25fe178d0a7d82acc2559448e02
Miles Dame <milesedame@gmail.com>
2024-12-10 15:20:09 -0500

    Update gunicorn

--- start_gunicorn.sh
+++ start_gunicorn.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cd /Users/rdrucker/Documents/match_game/MatchGameInfo/app
+cd /Users/rdrucker/Documents/dolphin/app
 source ../mgvenv/bin/activate
-gunicorn --reload --workers 3 --capture-output --log-level debug --bind unix:matchgame.sock -m 007 wsgi:app
+gunicorn --reload --workers 3 --capture-output --log-level debug --bind unix:dolphin.sock -m 007 wsgi:app
